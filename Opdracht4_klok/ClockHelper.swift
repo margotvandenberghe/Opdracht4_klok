29c3c1b4b283f46cf0ac9867cf1b807f4b7b3d95
Margot Vandenberghe <margotvandenberghe@margots-imac.lan>
2018-10-07 07:43:55 -0700

    layout done

---
+++ Opdracht4_klok/ClockHelper.swift
@@ -0,0 +1,13 @@
+//
+//  ClockHelper.swift
+//  Opdracht4_klok
+//
+//  Created by Margot Vandenberghe on 07/10/2018.
+//  Copyright © 2018 Vives. All rights reserved.
+//
+
+import Foundation
+
+class ClockHelper {
+    
+}
--- Opdracht4_klok/ViewController.swift
+++ Opdracht4_klok/ViewController.swift
@@ -9,7 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var wordHet: [UILabel]!
+    @IBOutlet var wordIs: [UILabel]!
+    
+    @IBOutlet var wordVijf: [UILabel]!
+    @IBOutlet var wordTien: [UILabel]!
+    
+    @IBOutlet var wordOver: [UILabel]!
+    @IBOutlet var wordVoor: [UILabel]!
+    
+    @IBOutlet var wordKwart: [UILabel]!
+    @IBOutlet var wordHalf: [UILabel]!
+    
+    //uren
+    @IBOutlet var wordEen: [UILabel]!
+    @IBOutlet var wordTwee: [UILabel]!
+    @IBOutlet var wordDrie: [UILabel]!
+    @IBOutlet var wordVier: [UILabel]!
+    @IBOutlet var wordVijf2: [UILabel]!
+    @IBOutlet var wordZes: [UILabel]!
+    @IBOutlet var wordZeven: [UILabel]!
+    @IBOutlet var wordAcht: [UILabel]!
+    @IBOutlet var wordNegen: [UILabel]!
+    @IBOutlet var wordTien2: [UILabel]!
+    @IBOutlet var wordElf: [UILabel]!
+    @IBOutlet var wordTwaalf: [UILabel]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
