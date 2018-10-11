d31dfb29672dd3c7efb5583b1be8510745b61c03
student <student@gebruikers-mac-mini-2.local>
2018-10-11 10:44:08 +0200

    logica

--- Opdracht4_klok/ClockHelper.swift
+++ Opdracht4_klok/ClockHelper.swift
@@ -8,41 +8,140 @@
 
 import Foundation
 
-class ClockHelper {
+class ClockHelper : IntClockHelp {
+    
+    var timer: Timer
     
     let formatter: DateFormatter;
     var currentTime: Array<String> = [] ;
     
     init() {
         formatter = DateFormatter()
+        if #available(iOS 10.0, *) {
+            timer = Timer.scheduledTimer(withTimeInterval: 300, repeats: true) {
+                (timer) in self.sendArray(timer: timer)
+            }
+        } else {
+            // Fallback on earlier versions
+        }
     }
     
-    func getHours () -> String {
+    func getHours () -> Int {
         formatter.dateFormat = "HH"
         let hourString = formatter.string(from: Date())
-        return hourString
+        return Int(hourString) ?? 0
     }
     
-    func getMinutes () -> String {
+    func getMinutes () -> Int {
         formatter.dateFormat = "mm"
-        var minutesString = formatter.string(from: Date())
-        let test = Int(minutesString) ?? 0
+        let minutesString = formatter.string(from: Date())
+        var test = Int(minutesString) ?? 0
         if (test % 5) != 0 {
             let r = test-(test % 5)
             let s = r/5
-            minutesString = String(s*5)
+            test = s*5
         }
-        return minutesString
+        return test
     }
     
-    func getArrayOfWords() -> Array<String> {
+    func getArrayOfWordsMinutes(){
+       switch getMinutes() {
+        case 5:
+            currentTime.append("VIJF")
+            currentTime.append("OVER")
+        case 10:
+            currentTime.append("TIEN")
+            currentTime.append("OVER")
+        case 15:
+            currentTime.append("KWART")
+            currentTime.append("OVER")
+        case 20:
+            currentTime.append("TIEN")
+            currentTime.append("VOOR")
+            currentTime.append("HALF")
+        case 25:
+            currentTime.append("VIJF")
+            currentTime.append("VOOR")
+            currentTime.append("HALF")
+        case 30:
+            currentTime.append("HALF")
+        case 35:
+            currentTime.append("VIJF")
+            currentTime.append("OVER")
+            currentTime.append("HALF")
+        case 40:
+            currentTime.append("TIEN")
+            currentTime.append("OVER")
+            currentTime.append("HALF")
+        case 45:
+            currentTime.append("KWART")
+            currentTime.append("VOOR")
+        case 50:
+            currentTime.append("TIEN")
+            currentTime.append("VOOR")
+        case 55:
+            currentTime.append("VIJF")
+            currentTime.append("VOOR")
+        case 0:
+            print(0)
+        default:
+            break
+        }
         
-        
-        return currentTime
     }
 
+    func getArrayOfWordsHours() {
+        
+        switch getHours() {
+        case 1:
+            currentTime.append("EEN")
+        case 2:
+            currentTime.append("TWEE")
+        case 3:
+            currentTime.append("DRIE")
+        case 4:
+            currentTime.append("VIER")
+        case 5:
+            currentTime.append("VIJFUUR")
+        case 6:
+            currentTime.append("ZES")
+        case 7:
+            currentTime.append("ZEVEN")
+        case 8:
+            currentTime.append("ACHT")
+        case 9:
+            currentTime.append("NEGEN")
+        case 10:
+            currentTime.append("TIENUUR")
+        case 11:
+            currentTime.append("ELF")
+        case 12:
+            currentTime.append("TWAALF")
+        default:
+            break
+        }
+    }
+
+    func sendArray(timer: Timer) -> Array<String> {
+        currentTime.append("HET")
+        currentTime.append("IS")
+        
+        getArrayOfWordsMinutes()
+        getArrayOfWordsHours()
+        
+        let help = currentTime
+        currentTime.removeAll()
+        
+        return help
+    }
     
     
     
 }
 
+protocol IntClockHelp {
+    
+    var timer: Timer { get set }
+    
+    func sendArray(timer: Timer) -> Array<String>
+}
--- Opdracht4_klok/ViewController.swift
+++ Opdracht4_klok/ViewController.swift
@@ -8,8 +8,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, IntClockHelp {
     
+    var timer: Timer
     
     @IBOutlet var wordHet: [UILabel]!
     @IBOutlet var wordIs: [UILabel]!
@@ -37,11 +38,72 @@ class ViewController: UIViewController {
     @IBOutlet var wordElf: [UILabel]!
     @IBOutlet var wordTwaalf: [UILabel]!
     
+    let clockhelper = ClockHelper()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        sendArray(timer: timer)
+    }
+    
+ 
+    func selectWords (arr: Array<String>) {
+        for a in arr {
+            switch a {
+            case "HET":
+                setInBlack(coll: wordHet)
+            case "IS":
+                setInBlack(coll: wordIs)
+            case "VIJF":
+                    setInBlack(coll: wordVijf)
+            case "TIEN":
+                setInBlack(coll: wordTien)
+            case "OVER":
+                setInBlack(coll: wordOver)
+            case "VOOR":
+                setInBlack(coll: wordVoor)
+            case "KWART":
+                setInBlack(coll: wordKwart)
+            case "HALF":
+                setInBlack(coll: wordHalf)
+            case "EEN":
+                setInBlack(coll: wordEen)
+            case "TWEE":
+                setInBlack(coll: wordTwee)
+            case "DRIE":
+                setInBlack(coll: wordDrie)
+            case "VIER":
+                setInBlack(coll: wordVier)
+            case "VIJFUUR":
+                setInBlack(coll: wordVijf2)
+            case "ZES":
+                setInBlack(coll: wordZes)
+            case "ZEVEN":
+                setInBlack(coll: wordZeven)
+            case "ACHT":
+                setInBlack(coll: wordAcht)
+            case "NEGEN":
+                setInBlack(coll: wordNegen)
+            case "TIENUUR":
+                setInBlack(coll: wordTien2)
+            case "ELF":
+                setInBlack(coll: wordElf)
+            case "TWAALF":
+                setInBlack(coll: wordTwaalf)
+            default:
+                break
+            }
+        }
+    }
+    
+    func setInBlack(coll: [UILabel]) {
+        for e in coll {
+            e.textColor = UIColor.black
+        }
+    }
+    
+    func sendArray(timer: Timer) -> Array<String> {
+        selectWords(arr: clockhelper.sendArray(timer: timer))
     }
 }
 
